a2847efbe88ade2001dbeb1fd5172c287e193014
zsmalla <malla7454@gmail.com>
2022-03-17 22:08:23 +0900

    Static file serving in Django

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('photo.urls')),     # 1차 url, 2차로 넘겨주기
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# DEBUG 모드일 때 - static 기능을 사용한다.
+# 배포시
+# 1. 미디어 파일 서버를 별도로 두고 사용한다.
+# 2. 웹서버에서 별도로 서빙 설정을 한다.
