b1e2535af4356d58f67c02b56b638684eee4d482
zsmalla <malla7454@gmail.com>
2022-03-17 22:34:35 +0900

    startapp accounts

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('photo.urls')),     # 1차 url, 2차로 넘겨주기
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
